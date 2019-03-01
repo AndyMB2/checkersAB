0b3992e6bd8b2c4a87cee6e7a9c249b907dcc808
csPython <cspython@AD25TF04NGF1J-178.local>
2019-03-01 09:21:42 -0500

    Added 1st Sqr and created Graphic Window

--- checkersAB.py
+++ checkersAB.py
@@ -3,6 +3,8 @@
 from graphics import *
 
 sqrSz = 70
+sqr = Rectangle(Point(sqrSz * 1, sqrSz * 1), Point(sqrSz * 2, sqrSz * 2))
 
 chkWin = GraphWin("Checkers", sqrSz * 10, sqrSz * 10)
 chkWin.setCoords(0, 0, sqrSz * 10, sqrSz * 10) 
+sqr.draw(chkWin)
