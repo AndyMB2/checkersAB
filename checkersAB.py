c38f1c8b70ffe99f3893efb19fe275e095fbd1b3
csPython <cspython@AD25TF04NGF1J-178.local>
2019-03-08 09:28:39 -0500

    made a row of squares diagonally

--- checkersAB.py
+++ checkersAB.py
@@ -2,9 +2,12 @@
 
 from graphics import *
 
-sqrSz = 70
-sqr = Rectangle(Point(sqrSz * 1, sqrSz * 1), Point(sqrSz * 2, sqrSz * 2))
+sqrSz = 50
 
 chkWin = GraphWin("Checkers", sqrSz * 10, sqrSz * 10)
-chkWin.setCoords(0, 0, sqrSz * 10, sqrSz * 10) 
-sqr.draw(chkWin)
+chkWin.setCoords(0, 0, sqrSz * 10, sqrSz * 10)
+
+for i in range(8):
+    sqr = Rectangle(Point(sqrSz * (i + 1), sqrSz * (i + 1)),
+                          Point(sqrSz * (i + 2), sqrSz * (i + 2)))
+    sqr.draw(chkWin)
