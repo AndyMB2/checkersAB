00f13ed4e1e5bbf2930f6394f10ed728e2b64af7
csPython <cspython@AD25TF04NGF1J-178.local>
2019-03-08 09:44:22 -0500

    created the rest of the rows for checker board

--- checkersAB.py
+++ checkersAB.py
@@ -7,6 +7,8 @@ sqrSz = 50
 chkWin = GraphWin("Checkers", sqrSz * 10, sqrSz * 10)
 chkWin.setCoords(0, 0, sqrSz * 10, sqrSz * 10)
 
-for i in range(8):
-    sqr = Rectangle(Point(sqrSz * (i + 1), sqrSz * 1), Point(sqrSz * (i + 2), sqrSz * 2))
-    sqr.draw(chkWin)
+for j in range(8):
+    for i in range(8):
+        sqr = Rectangle(Point(sqrSz * (i + 1), sqrSz * (j + 1)),
+                        Point(sqrSz * (i + 2), sqrSz * (j + 2)))
+        sqr.draw(chkWin)
