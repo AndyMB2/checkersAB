cc194de73d104d791924937fd9019ef688b947a8
csPython <cspython@AD25TF04NGF1J-178.local>
2019-03-12 13:23:08 -0400

    nest loop for grid and color

--- checkersAB.py
+++ checkersAB.py
@@ -7,4 +7,17 @@ def draw_sq(sY, sX, sqSize, color, win):
     square.setFill(color)
     square.draw(win)
 
-  
+sqrSz = 50
+sqCol = "red"
+
+chkWin = GraphWin("Checkers", sqrSz * 10, sqrSz * 10)
+chkWin.setCoords(0, 0, sqrSz * 10, sqrSz * 10)
+
+for j in range(8):
+    for i in range(8):
+        if(i + j) % 2 == 0:
+            sqCol = "red"
+        else:
+            sqCol = "black"
+        draw_sq(sqrSz * (i + 1), sqrSz * (j + 1), sqrSz, sqCol, chkWin)
+
