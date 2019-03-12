4e05cc073717a28c459f2e3213f02661dd8c5991
csPython <cspython@AD25TF04NGF1J-178.local>
2019-03-12 13:21:24 -0400

    created custom square function

--- checkersAB.py
+++ checkersAB.py
@@ -2,13 +2,9 @@
 
 from graphics import *
 
-sqrSz = 50
+def draw_sq(sY, sX, sqSize, color, win):
+    square = Rectangle(Point(sX, sY), Point(sX + sqSize, sY + sqSize))
+    square.setFill(color)
+    square.draw(win)
 
-chkWin = GraphWin("Checkers", sqrSz * 10, sqrSz * 10)
-chkWin.setCoords(0, 0, sqrSz * 10, sqrSz * 10)
-
-for j in range(8):
-    for i in range(8):
-        sqr = Rectangle(Point(sqrSz * (i + 1), sqrSz * (j + 1)),
-                        Point(sqrSz * (i + 2), sqrSz * (j + 2)))
-        sqr.draw(chkWin)
+  
